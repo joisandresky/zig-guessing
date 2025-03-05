1d86411de0ebd2ed894328fde3b4abf278c3080b
Jois Andresky <andreskyjois8@gmail.com>
2025-03-05 23:55:55 +0700

    works

---
+++ src/main.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    const now = std.time.nanoTimestamp();
+    var rng = std.rand.DefaultPrng.init(@intCast(now));
+
+    const target = rng.random().intRangeLessThan(i32, 1, 100);
+
+    try stdout.print("I'm thinking of a number between 1 and 100\n", .{});
+
+    var guess: i32 = 0;
+    var attempt: i32 = 0;
+
+    while (guess != target) {
+        try stdout.print("Enter your guess (1-100): ", .{});
+
+        const buf = try std.heap.page_allocator.alloc(u8, 64);
+        const input = try std.io.getStdIn().reader().readUntilDelimiter(buf, '\n');
+        defer std.heap.page_allocator.free(buf);
+
+        guess = try std.fmt.parseInt(i32, input, 10);
+
+        attempt += 1;
+
+        try stdout.print("You guessed {d}\n", .{guess});
+
+        if (guess < target) {
+            try stdout.print("Too Low!\n", .{});
+        } else if (guess > target) {
+            try stdout.print("Too High!\n", .{});
+        } else {
+            try stdout.print("Correct! You took {d} attempts\n", .{attempt});
+        }
+    }
+}
